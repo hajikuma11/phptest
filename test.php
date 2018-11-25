f92fa93597f9324cd5056df397eb5bac8316d177
hajikuma11 <hajimekuma11@gmail.com>
2018-11-25 12:01:21 +0900

    Update test.php

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+// URLを指定
+$url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
+
+file_get_contents($url."?p1=AAA&p2=BBB&p3=CCC&p4=DDDD");
